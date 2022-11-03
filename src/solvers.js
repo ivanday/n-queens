08365f7ac9915f1e120eea2d16b7f108228c9179
Ivan Day <ivanday@utexas.edu>
2022-11-03 13:07:47 -0500

    Implement find Rook Solution and find Rook Count

--- src/solvers.js
+++ src/solvers.js
@@ -16,16 +16,79 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var matrix = [];
+  for (var i = 0; i < n; i++) {
+    matrix.push([]);
+    for (var j = 0; j < n; j++) {
+      matrix[i].push(0);
+    }
+  }
+  var board = new Board(matrix);
+  // console.log(board.rows());
+  //var
+  // variable found = false;
+  var recursiveHelper = function (colIndex) {
+    if (colIndex === n) {
+      return;
+    } else {
+      for (var i = 0; i < n; i++) {
+        //setPiece
+        board.togglePiece(i, colIndex);
+        if (board.hasAnyRooksConflicts()) {
+          board.togglePiece(i, colIndex);
+          //recursiveHelper(colIndex + 1);
+        } else {
+          recursiveHelper(colIndex + 1);
+        }
+
+      }
+    }
+  };
+  // colIndex = 0; loop over rows; colIndex++
+    // set piece
+    // check to see if conflicts === false
+    // if false recurse rowIndex increased ++
+    // set piece;
+  recursiveHelper(0);
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board.rows()));
+  return board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
 
+  var matrix = [];
+  for (var i = 0; i < n; i++) {
+    matrix.push([]);
+    for (var j = 0; j < n; j++) {
+      matrix[i].push(0);
+    }
+  }
+  var board = new Board(matrix);
+
+  var recursiveHelper = function (colIndex) {
+    if (colIndex === n) {
+      solutionCount++;
+      return;
+    } else {
+      for (var i = 0; i < n; i++) {
+        board.togglePiece(i, colIndex);
+        if (!board.hasAnyRooksConflicts()) {
+          // board.togglePiece(i, colIndex);
+          recursiveHelper(colIndex + 1);
+        }
+        board.togglePiece(i, colIndex);
+
+        //add a piece
+        //run recursively
+        //remove the piece
+
+      }
+    }
+  };
+  recursiveHelper(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
